ceeccf92b6a32a4315a60a135c9936f34f11f3cf
Slean <s.dubuc@rubika-edu.com>
2020-03-03 18:23:15 +0100

    Texture Scaler

---
+++ Assets/Scripts/Utilities/TextureScaler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TextureScaler : MonoBehaviour
+{
+    public Transform self;
+    public Renderer rd;
+
+    private void Update()
+    {
+        rd.material.SetFloat("Vector1_D5FBFB1E", self.localScale.x );
+        rd.material.SetFloat("Vector1_D8985208", self.localScale.y);
+        rd.material.SetFloat("Vector1_1AE57C88", self.localScale.z);
+    }
+
+}
