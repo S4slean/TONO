693ca58da4aed57d11747bcd709665f0be2c6a39
Slean <s.dubuc@rubika-edu.com>
2020-03-03 17:51:50 +0100

    Enemies Data + Projectile (Early) + Scene Stan

---
+++ Assets/Scripts/Gameplay/Barrel/Barrel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel : MonoBehaviour
+{
+    public enum BarrelContent { empty, alcool, gunpowder};
+
+    public bool standing = true;
+    public BarrelContent content = BarrelContent.empty;
+
+    public virtual void Break()
+    {
+
+    }
+
+    public virtual void Drink()
+    {
+
+    }
+
+    public virtual void Kick(Vector3 direction)
+    {
+
+    }
+
+    public virtual void Throw(Vector3 direction, float distance)
+    {
+
+    }
+
+    public virtual void Explode()
+    {
+
+    }
+    
+
+
+}
