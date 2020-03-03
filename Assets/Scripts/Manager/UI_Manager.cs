5dfab420e881a1767b576a1d0f7325ecb5445459
Francois <f.noel@rubika-edu.com>
2020-03-03 20:38:35 +0100

    UI in progress

---
+++ Assets/Scripts/Manager/UI_Manager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIMode
+{
+    onSelection,
+    onTarget,
+}
+
+
+
+public class UI_Manager : MonoBehaviour
+{
+    [Header("Cursor Variables")]
+    public CursorIcon cursorPreset;
+
+    /// <summary>
+    /// Player Character actions
+    /// </summary>
+    public enum CursorIconMode
+    {
+        Invisible,
+        Wait,
+        MoveTo,
+        Select,
+        Fire,
+        Pass,
+        Reload,
+        Throw
+    }
+
+
+
+
+    /// <summary>
+    /// Display character Info UI
+    /// </summary>
+    /// <param name="characterPosition">Player Character concerned</param>
+    public void DisplayCharacterInfo(/*PlayerCharacter*/)
+    {
+
+    }
+
+    /// <summary>
+    /// Hide character info UI
+    /// </summary>
+    public void HideCharacterInfo(/*PlayerCharacter*/)
+    {
+
+    }
+
+
+    public void SetCursorIcon(CursorIconMode icon)
+    {
+        switch(icon)
+        {
+            case CursorIconMode.Select:
+                Cursor.SetCursor(cursorPreset.cursorIcons[0], Vector2.zero, CursorMode.Auto);
+                break;
+
+            case CursorIconMode.MoveTo:
+                Cursor.SetCursor(cursorPreset.cursorIcons[0], Vector2.zero, CursorMode.Auto);
+                break;
+
+            case CursorIconMode.Fire:
+                Cursor.SetCursor(cursorPreset.cursorIcons[0], Vector2.zero, CursorMode.Auto);
+                break;
+
+            case CursorIconMode.Invisible:
+                Cursor.SetCursor(Texture2D.whiteTexture, Vector2.zero, CursorMode.Auto);
+                break;
+        }
+    }
+
+    public void DisplayRoundPanel()
+    {
+
+    }
+
+    public void SetNextPhase()
+    {
+
+    }
+}
