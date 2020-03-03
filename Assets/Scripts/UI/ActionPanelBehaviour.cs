cec752a2e14d221f43d2868dbbb312622242e314
Francois <f.noel@rubika-edu.com>
2020-03-03 19:19:50 +0100

    UI in progress

---
+++ Assets/Scripts/UI/ActionPanelBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionPanelBehaviour : MonoBehaviour
+{
+    [Header("Panel References")]
+    public GameObject actionButtonPrefab;
+    public RectTransform rect;
+    float actionSpace = 17f;
+
+    public void ShowPanelAction(Vector2 panelPos)
+    {
+        rect.anchoredPosition = panelPos;
+
+        /*
+         * Check player character possibilities
+         * 
+         * ON PLAYER________
+         * RELOAD - if currentNumberOfBullets < totalNumberOfBullets && enough PA
+         * ATTACK - if ennemy near && enough PA
+         * THROW - if object near && enough PA
+         * PASS - if passable object near && enough PA
+         * WAIT
+         * 
+         * ON ENNEMY________
+         * THROW - if enough PA
+         * ATTACK
+         * 
+         * ON INTERACTABLE OBJECT/COMBUSTIBLE_______
+         * WAIT
+         * FIRE - if enough bullet
+         * 
+         * ON EMPTY________
+         * WAIT
+         * 
+         * ON ALL
+         * CANCEL
+         */
+    }
+}
---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum UIMode
+{
+    onSelection,
+    onTarget,
+}
+
+
+
+public class UI_Manager : MonoBehaviour
+{
+    [Header("Cursor Variables")]
+    public 
+
+    /// <summary>
+    /// Player Character actions
+    /// </summary>
+    public enum CursorIcon
+    {
+        Invisible,
+        Wait,
+        MoveTo,
+        Select,
+        Attack,
+        Hold,
+        Overthrow,
+        Throw
+    }
+
+    int currentNumberOfTurn = 0;
+
+
+
+
+
+    /// <summary>
+    /// Display character Info UI
+    /// </summary>
+    /// <param name="characterPosition">Player Character concerned</param>
+    public void DisplayCharacterInfo(/*PlayerCharacter*/)
+    {
+
+    }
+
+    /// <summary>
+    /// Hide character info UI
+    /// </summary>
+    public void HideCharacterInfo(/*PlayerCharacter*/)
+    {
+
+    }
+
+    public void SetCursorIcon(CursorIcon icon)
+    {
+
+    }
+
+
+
+}
