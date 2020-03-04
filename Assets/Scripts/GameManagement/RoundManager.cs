4423db09de74d06e5e992ec087cda373904b6d38
Permilias <visseboulon.engrenaze@gmail.com>
2020-03-04 18:42:49 +0100

    Save System

---
+++ Assets/Scripts/GameManagement/RoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    public static RoundManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public int currentRound;
+    public int currentTurn;
+
+    public void StartGame()
+    {
+        currentRound = -1;
+        currentTurn = -1;
+    }
+
+    public void NextRound()
+    {
+        currentRound++;
+    }
+
+    public void NextTurn()
+    {
+        currentTurn++;
+    }
+
+
+}
