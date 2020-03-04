47e416d1865e1bc07413689b89d532b63f51bab1
Slean <s.dubuc@rubika-edu.com>
2020-03-04 19:32:04 +0100

    enemyBehaviour (empty)

--- Assets/Scripts/Game Pawn/Character/Enemies/EnemieBehaviour.cs
+++ Assets/Scripts/Game Pawn/Character/Enemies/EnemieBehaviour.cs
@@ -2,6 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemieBehaviour : MonoBehaviour
+public class EnemieBehaviour : GamePawn
 {
+    public EnemyData enemyStats;
+
+    public int health = 1;
+    public int movementPoints = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        movementPoints = enemyStats.movement;
+        health = enemyStats.health;
+    }
+
+    public void PlayTurn()
+    {
+
+    }
+
+    public void GetClose(Tile tile)
+    {
+
+    }
+
+    public void GetInRange()
+    {
+
+    }
+
+    public void Drink()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void ThrowAlcohol()
+    {
+
+    }
 }
