f08c3a3d17c03a9d5e22d10647feeabf3f525c18
Permilias <visseboulon.engrenaze@gmail.com>
2020-03-05 18:44:51 +0100

    Level & Data Management

---
+++ Assets/Scripts/DataManagement/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LevelManager
+{
+    public static LevelProgress[] levelProgresses;
+
+    public static void InitializeLevelProgresses(LevelList levelList)
+    {
+        levelProgresses = new LevelProgress[levelList.levelDatas.Length];
+    }
+}
+
+public enum LevelTheme
+{
+    dark,
+    bright
+}
