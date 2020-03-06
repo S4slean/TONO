9c3081578393fa3b8344d9834dcee7ec6a0d4c6a
QuentinDenois <Quentin.denois@hotmail.fr>
2020-03-06 17:38:27 +0100

    Update getLine

--- Assets/Scripts/Tilemap/GridManager.cs
+++ Assets/Scripts/Tilemap/GridManager.cs
@@ -43,41 +43,150 @@ public class GridManager : MonoBehaviour
         }
     }
 
-    public List<Tile> GetLineUntilObstacle(List<Tile> line, Direction dir)
+    public List<Tile> GetLineUntilObstacle(Direction dir, Tile startingTile, bool throughWater = false)
     {
-        switch (dir)
+        List<Tile> line = new List<Tile>();
+        bool finding = true;
+        Tile currentTile = startingTile;
+
+        /*print("Up : " + currentTile.neighbours.up);
+        print("Right : " + currentTile.neighbours.right);
+        print("Down : " + currentTile.neighbours.down);
+        print("Left : " + currentTile.neighbours.left);*/
+
+        while (finding)
         {
-            case Direction.Up:
-                if (line[line.Count -1].neighbours.up != null && line[line.Count - 1].neighbours.up.isWalkable && (line[line.Count - 1].neighbours.up.GetPawnOnTile() == null || line[line.Count - 1].neighbours.up.GetPawnOnTile() == PlayerManager.instance.playerCharacter))
-                {
-                    line.Add(line[line.Count - 1].neighbours.up);
-                    GetLineUntilObstacle(line, dir);
-                }
+            switch (dir)
+            {
+                case Direction.Up:
+                    if(currentTile.neighbours.up != null)
+                    {
+                        if(currentTile.neighbours.up is Water)
+                        {
+                            if (throughWater)
+                            {
+                                currentTile = currentTile.neighbours.up;
+                                continue;
+                            }
+                            else
+                            {
+                                finding = false;
+                            }
+                        }
+                        else if(currentTile.neighbours.up.GetPawnOnTile() == null || currentTile.neighbours.up.GetPawnOnTile() is EnemieBehaviour)
+                        {
+                            line.Add(currentTile);
+                            currentTile = currentTile.neighbours.up;
+                            continue;
+                        }
+                        else
+                        {
+                            finding = false;
+                        }
+                    }
+                    else
+                    {
+                        finding = false;
+                    }
                     break;
 
-            case Direction.Right:
-                if (line[line.Count - 1].neighbours.right != null && line[line.Count - 1].neighbours.right.isWalkable && (line[line.Count - 1].neighbours.right.GetPawnOnTile() == null || line[line.Count - 1].neighbours.right.GetPawnOnTile() == PlayerManager.instance.playerCharacter))
-                {
-                    line.Add(line[line.Count - 1].neighbours.right);
-                    GetLineUntilObstacle(line, dir);
-                }
+                case Direction.Right:
+                    if(currentTile.neighbours.right != null)
+                    {
+                        if(currentTile.neighbours.right is Water)
+                        {
+                            if (throughWater)
+                            {
+                                currentTile = currentTile.neighbours.right;
+                                continue;
+                            }
+                            else
+                            {
+                                finding = false;
+                            }
+                        }
+                        else if(currentTile.neighbours.right.GetPawnOnTile() == null || currentTile.neighbours.right.GetPawnOnTile() is EnemieBehaviour)
+                        {
+                            line.Add(currentTile);
+                            currentTile = currentTile.neighbours.right;
+                            continue;
+                        }
+                        else
+                        {
+                            finding = false;
+                        }
+                    }
+                    else
+                    {
+                        finding = false;
+                    }
                     break;
 
-            case Direction.Down:
-                if (line[line.Count - 1].neighbours.down != null && line[line.Count - 1].neighbours.down.isWalkable && (line[line.Count - 1].neighbours.down.GetPawnOnTile() == null || line[line.Count - 1].neighbours.down.GetPawnOnTile() == PlayerManager.instance.playerCharacter))
-                {
-                    line.Add(line[line.Count - 1].neighbours.down);
-                    GetLineUntilObstacle(line, dir);
-                }
+                case Direction.Down:
+                    if(currentTile.neighbours.down != null)
+                    {
+                        if(currentTile.neighbours.down is Water)
+                        {
+                            if (throughWater)
+                            {
+                                currentTile = currentTile.neighbours.down;
+                                continue;
+                            }
+                            else
+                            {
+                                finding = false;
+                            }
+                        }
+                        else if(currentTile.neighbours.down.GetPawnOnTile() == null || currentTile.neighbours.down.GetPawnOnTile() is EnemieBehaviour)
+                        {
+                            line.Add(currentTile);
+                            currentTile = currentTile.neighbours.down;
+                            continue;
+                        }
+                        else
+                        {
+                            finding = false;
+                        }
+                    }
+                    else
+                    {
+                        finding = false;
+                    }
                     break;
 
-            case Direction.Left:
-                if (line[line.Count - 1].neighbours.left != null && line[line.Count - 1].neighbours.left.isWalkable && (line[line.Count - 1].neighbours.left.GetPawnOnTile() == null || line[line.Count - 1].neighbours.left.GetPawnOnTile() == PlayerManager.instance.playerCharacter))
-                {
-                    line.Add(line[line.Count - 1].neighbours.left);
-                    GetLineUntilObstacle(line, dir);
-                }
+                case Direction.Left:
+                    if(currentTile.neighbours.left != null)
+                    {
+                        if(currentTile.neighbours.left is Water)
+                        {
+                            if (throughWater)
+                            {
+                                currentTile = currentTile.neighbours.left;
+                                continue;
+                            }
+                            else
+                            {
+                                finding = false;
+                            }
+                        }
+                        else if(currentTile.neighbours.left.GetPawnOnTile() == null || currentTile.neighbours.left.GetPawnOnTile() is EnemieBehaviour)
+                        {
+                            line.Add(currentTile);
+                            currentTile = currentTile.neighbours.left;
+                            continue;
+                        }
+                        else
+                        {
+                            finding = false;
+                        }
+                    }
+                    else
+                    {
+                        finding = false;
+                    }
                     break;
+
+            }
         }
         return line;
     }
