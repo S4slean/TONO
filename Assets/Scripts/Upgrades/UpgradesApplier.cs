eb8653027ea6079d7ff307d912b11c8187a170cd
Permilias <visseboulon.engrenaze@gmail.com>
2020-03-09 10:44:09 +0100

    Pause Fix

---
+++ Assets/Scripts/Upgrades/UpgradesApplier.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradesApplier : MonoBehaviour
+{
+    public static UpgradesApplier Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public bool[] upgradesPossessed;
+
+
+}
