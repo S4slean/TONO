8118e86fe42876eaaa5011aabd68b36eba20aa25
Permilias <visseboulon.engrenaze@gmail.com>
2020-03-11 20:32:23 +0100

    Cinematics Start

---
+++ Assets/Scripts/Cinematics/CinematicDialogueDisplay.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CinematicDialogueDisplay : MonoBehaviour
+{
+    public GameObject dialogueDisplay;
+    public TextMeshProUGUI dialogueTextMesh;
+
+    public static CinematicDialogueDisplay Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        dialogueDisplay.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (count <= 0) return;
+
+        count -= Time.deltaTime;
+        if(count <= 0)
+        {
+            count = 0f;
+            HideDialogue();
+        }
+    }
+
+    public float dialogueDuration;
+    float count;
+    public void DisplayDialogue(string dialogue)
+    {
+        count = dialogueDuration;
+        dialogueTextMesh.text = dialogue;
+        dialogueDisplay.SetActive(true);
+    }
+
+    public void HideDialogue()
+    {
+        dialogueDisplay.SetActive(false);
+    }
+}
