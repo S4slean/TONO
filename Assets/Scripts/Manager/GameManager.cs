d41549295e6a477b0fe2aa4d1ddce77fe42249ca
Francois <f.noel@rubika-edu.com>
2020-03-12 11:30:50 +0100

    ui in progress
    fixing portraits animation

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -54,6 +54,8 @@ public class GameManager : MonoBehaviour
         UI_Manager.instance.messagePanel.SetUI();
         UI_Manager.instance.pausePanel.SetUI();
 
+        UI_Manager.instance.actionPanel.ResetPanelAction();
+
         UI_Manager.instance.SetUIDisplayModeOn(UIDisplayMode.Start);
 
         StartBombardmentTurn();
