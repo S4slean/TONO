26b7b28e5882150a75271f01d434a55d81bfe5f8
Slean <s.dubuc@rubika-edu.com>
2020-03-13 09:52:23 +0100

    Prot

---
+++ Assets/Scripts/DataManagement/LevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public LevelList levelList;
+
+    GameObject level;
+    GameObject background;
+
+    public void LoadLevel()
+    {
+        level = levelList.levelDatas[LevelManager.currentLevel].level;
+        background = levelList.levelDatas[LevelManager.currentLevel].background;
+    }
+}
