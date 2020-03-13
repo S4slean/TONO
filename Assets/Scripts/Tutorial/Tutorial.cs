8f372ef894bc03b64d5af1f1e6fe6f1b772354f3
Permilias <visseboulon.engrenaze@gmail.com>
2020-03-13 09:11:52 +0100

    Tutorial Display +

---
+++ Assets/Scripts/Tutorial/Tutorial.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tutorial", menuName = "Tutorials/New Tutorial", order = 0)]
+public class TutorialData : ScriptableObject
+{
+    public string tutorialText;
+    public Vector2 tutorialTextPosition;
+
+    public Vector2 tutorialArrowPosition;
+    [Range(0, 360)]
+    public float arrowRotation;
+}
